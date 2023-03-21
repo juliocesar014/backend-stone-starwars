01e9330e7fd8d008b1b6d5cd4a15e7f2e9ed6395
juliocesar014 <juliocesar014@live.com>
2023-03-21 09:31:10 -0300

    feat: Add Swagger

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- setup/urls.py
+++ setup/urls.py
@@ -5,6 +5,21 @@ from store.views import ProductViewSet, ClientViewSet, BuyViewSet, BuyHistoryAPI
 from rest_framework import routers
 from rest_framework import permissions
 from store import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Docs",
+      default_version='v1',
+      description="API STARS WARS STORE (STONE CHALLENGE))",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="juliocesar014@live.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSet, basename='Products')
@@ -16,7 +31,8 @@ router.register('buys', BuyViewSet, basename='Buys')
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('history/<int:client_id>/', ClientHistoryView.as_view(), name='client_history'),
-    path('', include(router.urls)),
-    path('history/', views.BuyHistoryAPIView.as_view(), name='history'),
+    path('starstore/', include(router.urls)),
+    path('starstore/history/', views.BuyHistoryAPIView.as_view(), name='history'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    
 ]
